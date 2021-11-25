99a3a6081de1b9d9e780762cf95b8137c3c9b234
Yan Kurbatov <deknowny@gmail.com>
2021-11-25 23:02:09 +0300

    Fix project slug dir

--- hooks/pre_gen_project.py
+++
@@ -1,6 +0,0 @@
-text = """
-Check out
-https://github.com/deknowny/python-package-template/blob/main/COOKIECUTTER-EXPLANATION.md
-for understanding fields are requested here
-""".strip()
-print(text)
