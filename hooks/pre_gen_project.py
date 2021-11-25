ce81264d70bcf13ed49c3e9deddfeafbcd354ab3
Yan Kurbatov <deknowny@gmail.com>
2021-11-25 22:41:12 +0300

    Add pre gen hook

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,6 @@
+text = """
+Check out
+https://github.com/deknowny/python-package-template/blob/main/COOKIECUTTER-EXPLANATION.md
+for understanding fields are requested here
+""".strip()
+print(text)
