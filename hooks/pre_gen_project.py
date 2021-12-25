086ad42d5f98679a723899fb5ffcb891db35095a
Yan Kurbatov <deknowny@gmail.com>
2021-12-25 19:32:54 +0300

    Add exit(1) for bad case

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,8 +4,9 @@ try:
     import pymake
 except ImportError:
     print(
-        "Error: `py-make` is required. "
+        "\n\nError: `py-make` is required. "
         "Run `{} -m pip install py-make`".format(
             sys.executable
         )
     )
+    exit(1)
